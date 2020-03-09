efc7da616281cd68a3ea2fd6f7651a11f2c258e7
AidanLincoln <aidantlincoln@gmail.com>
2020-03-09 15:00:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+let main = document.querySelector("main#main")
+main.remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+// OR newHeader.innerHTML("<class id='victory'>")
+newHeader.innerHTML = 'YOUR-NAME is the champion'
+console.dir(newHeader)
