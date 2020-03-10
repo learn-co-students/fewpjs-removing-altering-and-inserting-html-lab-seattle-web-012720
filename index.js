c15a1df9f6f60f308c84c77f28396df7a529cd86
github username <github email address>
2020-03-10 09:08:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Jazz is the champion"
+
+// let newHeader = document.querySelector('head')
+// newHeader.createElement('h1#victory')
