1ee2b25ec338d9f553925816ecb1c5ecd4454391
javila35 <josepharthuravila@gmail.com>
2020-03-10 08:44:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Joe is the champion';
